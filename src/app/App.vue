c5b1941c56393b6f3852eab671a59d81cab5e191
lottestu <lottestu@student.matnat.uio.no>
2021-11-18 14:32:23 +0100

    Create vue project

---
+++ src/app/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard">
+        <h1 class="dashboard__title">Our dashboard</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
