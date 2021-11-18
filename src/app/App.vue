fadb5de47cae199c3eb18993051bd2a6e15972c5
lottestu <lottestu@student.matnat.uio.no>
2021-11-18 15:52:34 +0100

    Started todolist component (not done)

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,18 +1,29 @@
 <template>
     <div class="dashboard">
-        <h1 class="dashboard__title">Our dashboard</h1>
+        <h1 class="dashboard__title">Our component dashboard</h1>
+        <div class="dashboard__components">
+            <div class="dashboard__component dashboard__toDoList"><ToDoList/></div>
+            <div class="dashboard__component">slideshow</div>
+            <div class="dashboard__component">kontaktskjema</div>
+            <div class="dashboard__component">sorterbar tabell</div>
+            <div class="dashboard__component">quiz-app</div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
+    import ToDoList from '../components/ToDoList.vue';
 
-            }
+    export default {
+        components: {
+            ToDoList
         }
+
     }
 </script>
 
 <style>
+    @import '../style/reset.css';
+    @import '../style/variables.css';
+    @import '../style/style.css';
 </style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="toDoList">
+        <h2 class="toDoList__title">To do list</h2>
+        <section class="toDoList__addTaskSection">
+            <input type="text" placeholder="Add task here" id="taskInput">
+            <button @click="addTask">Add Task</button>
+        </section>
+        
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                tasks: [],
+                addedTask: '' //remove?
+            }
+        },
+
+        methods: {
+            addTask() {
+
+            },
+            updateTaskView() {
+
+            },
+            taskChecked() {
+
+            },
+            removeTask(){
+
+            }
+
+        }
+    }
+    
+</script>
+
+<style>
+    @import '../style/reset.css';
+    @import '../style/variables.css';
+    @import '../style/style.css';
+</style>
