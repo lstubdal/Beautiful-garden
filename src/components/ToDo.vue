5aca17d00c80569f549bb1c4683b9980b73aeba4
lottestu <lottestu@student.matnat.uio.no>
2021-11-23 16:39:08 +0100

    Edit vw dashboard

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -1,34 +1,16 @@
 <template>
   <div class="toDo">
-    <h2 class="toDo__title">To do list</h2>
-
-    <section class="toDo__addTaskSection">
-      <input
-        v-model="newTask"
-        type="text"
-        placeholder="Add task here"
-        class="toDo__input"
-        @keyup.enter="addTask"
-      />
-      <!--@keyup.enter => addTask when enter is pused-->
-      <button @click="addTask" class="toDo__button">Add Task</button>
-      <div v-for="(task, index) in tasks" class="toDo__taskContainer">
-        <div class="toDo__task">{{ task.text }}</div>
-        <div class="toDo__remove">remove</div>
-      </div>
-      <!--button @click="addTask" class="toDoList__button">Add Task</button-->
-
-      <!--button @click="removeTask(index)" class="toDoList__remove">remove</button-->
-    </section>
-
-    <!--section class="toDoList__addTaskSection" >
-            <input v-model="newTask" type="text" placeholder="Add task here" id="taskInput" class="toDoList__input">
-            <button @click="addTask" class="toDoList__button">Add Task</button>
-            <div class="toDoList__task" v-for="task in tasks"> {{ task.text }}
-
-                <button @click="removeTask" class="toDoList__remove">remove</button>
+    <h2 class="toDo__title">flower stuff to do</h2>
+    <div class="toDo__taskContainer">
+        <div v-for="(task, index) in tasks" class="toDo__task">   
+            <button class="toDo__button"></button>
+            <div class="toDo__task">{{ task.text }}</div>
+            <div class="toDo__remove">
+                <img src="../../assets/trashIcon.png" alt="trash icon">
             </div>
-        </section-->
+      </div>
+      <input v-model="newTask" type="text"  class="toDo__input" placeholder=" +  Add task, push enter" @keyup.enter="addTask"> <!-- keyup.enter calls addTask function after pushed -->
+    </div>
   </div>
 </template>
 
@@ -40,7 +22,6 @@ export default {
       tasks: [],
     };
   },
-
   methods: {
     addTask() {
       if (this.newTask === "") {
@@ -48,83 +29,93 @@ export default {
         alert("Ups, you need to write a task!");
       } else {
         this.tasks.push({ text: this.newTask, done: false });
+        console.log(this.newTask)
         this.newTask = ""; // add task
       }
     },
     taskChecked() {},
     removeTask() {
-      console.log("remove button clicked");
 
-      const tasksOnScreen = document.querySelectorAll(".toDoList__remove");
-      [...tasksOnScreen].forEach((task) => {
-        console.log(); /* ikke bruk */
-      });
-      /*[...tasksOnScreen].forEach(taskOnScreen => {
-                    this.tasks.forEach(taskInList => {
-                        if (taskOnScreen.innerText === taskInList) {
-                            console.log('task to remove: ' + taskInList);
-                            console.log(this.tasks.length)
-                            this.tasks.pop(taskInList);
-                            console.log('task removed')
-                        
-                            
-                        }
-                    })
-                }) */
     },
   },
 };
 </script>
 
 <style>
+
 .toDo {
-  overflow: scroll;
-  height: 100%;
-  background-color: rgb(191, 202, 188);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 100%;
+    overflow: scroll;
+    background-color: #CFD0A9;
 }
 .toDo__title {
-  font-size: 1.5em;
-  text-align: center;
-  padding: 20px;
+    font-size: 1.1em;
+    font-family: var(--component-headline);
+    font-weight: bold;
+    margin-bottom: -3%;
 }
+
 .toDo__taskContainer {
-  display: flex;
-  text-decoration: none;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    height: 230px;
+    width: 60%;
+    position: relative; 
+    border-radius: 10px;
+    border: none;
+    background-color: white;
+    overflow: scroll;
+    padding-top: 2%;
+            
 }
 .toDo__input {
-  height: 35px;
-  width: 210px;
-  border-radius: 2px;
+    height: 30px;
+    width: 100%;
+    border-radius: 10px;
+    border: 1px solid lightgray;
+    position: absolute;         /* absolute in relation to toDo__taskContainer (parent) */
+    bottom: 0%;     /* fix: not at bottom when to many tasks */
+    padding-left: 15px;
 }
 .toDo__button {
-  display: flex;
-  justify-self: center;
-  background: none;
-  padding: 4px 10px 4px 10px;
-  font-size: 1em;
-  margin-left: 25%;
-  margin-top: 5%;
-  border-radius: 2px;
-  color: white;
-  background-color: rgb(33, 49, 46);
-  margin-bottom: 10%;
+    background-color: var(--light);
+    height: 16px;
+    width: 30px;
+    border-radius: 38%;
+    border: 2px solid black;
+    margin-left: 10%;
+}
+.toDo__button:hover {
+    background-color: #CFD0A9;
 }
 
 .toDo__task {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  background-color: beige;
-  font-family: arial;
-  font-size: 1.2em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    background-color: white;
+    font-family: arial;
+    font-size: 0.9em;
+    border-radius: 10px;
+    padding: 1% 5% 1% 5%;
+    bottom: 0%;
+
 }
 
 .toDo__remove {
-  background: none;
-  border: none;
-  cursor: pointer;
-  background-color: beige;
+        margin-right: 10%
 }
+
+.toDo__remove img {
+    height: 25px;
+    width: 30px;
+    cursor: pointer;
+}
+
 </style>
