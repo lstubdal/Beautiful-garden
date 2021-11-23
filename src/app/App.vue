27ca5eba677322ae1c6d1083d639a7abf6cba182
soelvi <solvisimensen@live.no>
2021-11-23 11:18:40 +0100

    add template

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,29 +1,28 @@
 <template>
-    <div class="dashboard">
-        <h1 class="dashboard__title">Our component dashboard</h1>
-        <div class="dashboard__components">
-            <div class="dashboard__component dashboard__toDoList"><ToDoList/></div>
-            <div class="dashboard__component">slideshow</div>
-            <div class="dashboard__component">kontaktskjema</div>
-            <div class="dashboard__component">sorterbar tabell</div>
-            <div class="dashboard__component">quiz-app</div>
-        </div>
+  <div class="dashboard">
+    <h1 class="dashboard__title">Our component dashboard</h1>
+    <div class="dashboard__container">
+      <div class="dashboard__component dashboard__toDoList"><ToDoList /></div>
+      <div class="dashboard__component">slideshow</div>
+      <div class="dashboard__component">kontaktskjema</div>
+      <div class="dashboard__component">sorterbar tabell</div>
+      <div class="dashboard__component">quiz-app</div>
     </div>
+  </div>
 </template>
 
 <script>
-    import ToDoList from '../components/ToDo.vue';
+import ToDoList from "../components/ToDo.vue";
 
-    export default {
-        components: {
-            ToDoList
-        }
-
-    }
+export default {
+  components: {
+    ToDoList,
+  },
+};
 </script>
 
 <style>
-    @import '../style/reset.css';
-    @import '../style/variables.css';
-    @import '../style/style.css';
+@import "../style/reset.css";
+@import "../style/variables.css";
+@import "../style/style.css";
 </style>
