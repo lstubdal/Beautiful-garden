902ea6db3013cca3c9f159ba09280736f5d63786
lottestu <lottestu@student.matnat.uio.no>
2021-11-29 22:33:10 +0100

    Create Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer class="footer">Beautiful Garden Footer</footer>
+</template>
+
+<script>
+
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style>
+    .footer {
+        width: 100vw;
+        height: 20vh;
+        background-color: rgb(87, 85, 85);
+        color: white;
+    }
+</style>
