96c0e7fa10dcc2f6a8d06b07d556a9e4e780d3f9
lottestu <lottestu@student.matnat.uio.no>
2021-11-30 19:33:11 +0100

    Style footer without store.js

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template>
-    <footer class="footer">Beautiful Garden Footer</footer>
+    <footer class="footer">
+        <RouterLink :to="{ name: 'home' }" class="footer__logo">f.logo</RouterLink>
+        <nav class="header__nav">
+            <RouterLink class="footer__page" v-for="page in pages" :to="{ name: page.title, params: {page_id: page.id }}">{{ page.title }}</RouterLink>
+        </nav>
+    </footer>
 </template>
 
 <script>
 
     export default {
         data() {
-            return {
-                
+            return {  
+                pages: [                                                         //array for navigation pages
+                    {title: 'To do list', id: 'to-do-list'},                    // id for routerlink => page_id
+                    {title: 'Help from experts', id: 'help-from-experts'},  
+                    {title: 'Flower slideshow', id: 'flower-slideshow'},
+                    {title: 'Flower quiz', id: 'flower-quiz'},
+                    {title: 'Flower overview', id: 'flower-overview'}
+                ]
             }
         }
     }
@@ -15,9 +26,31 @@
 
 <style>
     .footer {
+        display: flex;
+        justify-content: center;
+        align-items: center ;
         width: 100vw;
         height: 20vh;
         background-color: rgb(87, 85, 85);
         color: white;
+    }
+
+    .footer__logo {
+        position: absolute;
+        left: 0;
+        padding: 30px;
+        color: var(--light);
+        text-decoration: none;
+    }
+    .footer__page {
+        font-family: arial;
+        text-decoration: none;
+        color: var(--light);
+        padding-left: 15px;
+        padding-right: 15px;
+    }
+    .footer__page:hover {
+        color: #F08383;
+        text-decoration: underline 2px solid;
     }
 </style>
