b6be9e56c9fc9833440de859450155b6bba10f1d
soelvi <solvisimensen@live.no>
2021-12-05 15:00:47 +0100

    Add FormInput as own component

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,41 +1,50 @@
 <template>
 	<div class="contact-form">
-		<h2 class="contact-form__title">help from flower experts</h2>
-		
-		<form class="contact-form__input">
-			<div class="contact-form__error">
+		<h2 class="contact-form__title">{{ title }}</h2>
+		<div class="contact-form__error">
 				<p>{{ error }}</p>
 				<p class="contact-form__error">{{ firstNameValidation }}</p>
 				<p class="contact-form__error">{{ lastNameValidation }}</p>
 				<p class="contact-form__error">{{ emailValidation }}</p>
 				<p class="contact-form__error">{{ messageValidation }}</p>
-			</div>
-			<!-- <label for="name">Name</label> -->
+		</div>
+
+		<FormInput v-model="form.firstName"/>
+		<!-- <form class="contact-form__input">	
+			<label for="name">Name</label>
 			<div class="contact-form__fullName">
 				<input class="contact-form__firstName" type="text" placeholder="First name" v-model="form.firstName" />
 				
 				<input class="contact-form__lastName" type="text" placeholder="Last name" v-model="form.lastName" />
 			</div>
-			<!-- <label for="email">E-mail</label> -->
+			<label for="email">E-mail</label>
 			<input class="contact-form__email" type="email" placeholder="E-mail" v-model="form.email" />
-			<!-- <label for="message">Message</label> -->
+			<label for="message">Message</label>
 			<textarea class="contact-form__message" type="textarea" cols="1" placeholder="What do you need help with?" v-model="form.message" />
-		</form>
-		<button class="contact-form__button" @click.prevent="validateForm">Ask!</button>
+		</form> -->
+		<button class="contact-form__button" @click.prevent="validateForm">Ask!</button> <!-- the submit event will no longer reload the page -->
 	</div> 	
 </template>
 
 <script>
+	import FormInput from '../components/FormInput.vue'
+
 	export default {
+
+		components: {
+			FormInput,
+		}, 
+
 		data() {
 			return {
+				title: 'help from flower experts',
+				error: '',	
 				form: {
 					firstName: '',
 					lastName: '',
 					email: '',
 					message: '',
 				},
-				error: '',	
 			};
 		},
 
@@ -47,6 +56,9 @@
 				}
 				return this.error = 'Invalid form.';
 			},
+			resetForm() {
+				
+			}
 		},
 
 		computed: {
@@ -92,40 +104,9 @@
 		align-self: center;
 	}
 
-	.contact-form__input input {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		border: none;
-		border-radius: 10px;
-		padding: 1em;
-		margin-top: 0.5em;
-		width: 100%;
-	}
-
 	.contact-form__error {
 		position: relative;
 		color: darkred;
-	}
-
-	.contact-form__firstName {
-		margin-right: 0.5em;
-	}
-
-	.contact-form__fullName {
-		display: flex; 
-		align-items: center;
-	}
-
-	.contact-form__message {
-		font-family: var(--main-font);
-		resize: none;
-		border: none;
-		border-radius: 10px;
-		padding: 1em;
-		margin-top: 0.5em;
-		height: 60%;
-		width: 100%;
 	}
 
 	.contact-form__button {
