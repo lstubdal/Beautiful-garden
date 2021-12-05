4f4b29a9982d1c1783f9355503a748a2787af659
soelvi <solvisimensen@live.no>
2021-12-05 16:11:49 +0100

    minor change

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -9,8 +9,7 @@
 				<p class="contact-form__error">{{ messageValidation }}</p>
 		</div>
 
-		<FormInput v-model="form.firstName"/>
-		<!-- <form class="contact-form__input">	
+		<form class="contact-form__input">	
 			<label for="name">Name</label>
 			<div class="contact-form__fullName">
 				<input class="contact-form__firstName" type="text" placeholder="First name" v-model="form.firstName" />
@@ -21,19 +20,19 @@
 			<input class="contact-form__email" type="email" placeholder="E-mail" v-model="form.email" />
 			<label for="message">Message</label>
 			<textarea class="contact-form__message" type="textarea" cols="1" placeholder="What do you need help with?" v-model="form.message" />
-		</form> -->
+		</form>
 		<button class="contact-form__button" @click.prevent="validateForm">Ask!</button> <!-- the submit event will no longer reload the page -->
 	</div> 	
 </template>
 
 <script>
-	import FormInput from '../components/FormInput.vue'
+	// import FormInput from '../components/FormInput.vue'
 
 	export default {
 
-		components: {
-			FormInput,
-		}, 
+		// components: {
+		// 	FormInput,
+		// }, 
 
 		data() {
 			return {
@@ -107,6 +106,37 @@
 	.contact-form__error {
 		position: relative;
 		color: darkred;
+	}
+
+	.contact-form__input input {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		border: none;
+		border-radius: 10px;
+		padding: 1em;
+		margin-top: 0.5em;
+		width: 100%;
+	}
+
+	.contact-form__firstName {
+		margin-right: 0.5em;
+	}
+
+	.contact-form__fullName {
+		display: flex; 
+		align-items: center;
+	}
+
+	.contact-form__message {
+		font-family: var(--main-font);
+		resize: none;
+		border: none;
+		border-radius: 10px;
+		padding: 1em;
+		margin-top: 0.5em;
+		height: 60%;
+		width: 100%;
 	}
 
 	.contact-form__button {
--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -1,51 +1,8 @@
 <template>
-		<form class="contact-form__input">	
-			<div class="contact-form__fullName">
-				<input class="contact-form__firstName" type="text" placeholder="First name" v-model="form.firstName" />
-				<input class="contact-form__lastName" type="text" placeholder="Last name" v-model="form.lastName" />
-			</div>
-			<input class="contact-form__email" type="email" placeholder="E-mail" v-model="form.email" />
-			<textarea class="contact-form__message" type="textarea" cols="1" placeholder="What do you need help with?" v-model="form.message" />
-		</form>
 </template>
 
 <script>
-	export default {
-		props: {
-			input: String,
-		},
-	}
 </script>
 
 <style>
-	.contact-form__input input {
-		position: relative;
-		display: flex;
-		flex-direction: column;
-		border: none;
-		border-radius: 10px;
-		padding: 1em;
-		margin-top: 0.5em;
-		width: 100%;
-	}
-
-	.contact-form__firstName {
-		margin-right: 0.5em;
-	}
-
-	.contact-form__fullName {
-		display: flex; 
-		align-items: center;
-	}
-
-	.contact-form__message {
-		font-family: var(--main-font);
-		resize: none;
-		border: none;
-		border-radius: 10px;
-		padding: 1em;
-		margin-top: 0.5em;
-		height: 60%;
-		width: 100%;
-	}
 </style>
