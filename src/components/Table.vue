dab7aba87af564e26164ccf66e8609c460d8cfdc
soelvi <solvisimensen@live.no>
2021-12-06 13:12:06 +0100

    add flowerData to table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -5,7 +5,7 @@
 			<thead>
 				<tr>
 					<td>Type</td>
-					<td>Maintained</td>
+					<td>Last maintained</td>
 				</tr>
 			</thead>
 			<tbody>
@@ -18,12 +18,45 @@
 </template>
 
 <script>
-	export default{
+	const flowerData = `[{
+  "type": "Slender Yellow Woodsorrel",
+  "date": "19/06/2021"
+}, {
+  "type": "Muskroot",
+  "date": "06/06/2021"
+}, {
+  "type": "Script Lichen",
+  "date": "17/08/2022"
+}, {
+  "type": "Proliferating Bulrush",
+  "date": "07/08/2021"
+}, {
+  "type": "Shrubby Yellowcrest",
+  "date": "11/12/2021"
+}, {
+  "type": "San Diego Bur Ragweed",
+  "date": "08/10/2021"
+}, {
+  "type": "Cuero De Sapo",
+  "date": "14/04/2022"
+}, {
+  "type": "Castle Lake Bedstraw",
+  "date": "21/05/2021"
+}, {
+  "type": "Splendid Feather Moss",
+  "date": "21/01/2022"
+}, {
+  "type": "Bolander's Onion",
+  "date": "19/07/2022"
+}]`;
+
+	export default {
 		data() {
 			return {
 				title: 'all the flowers in the garden',
+				content: JSON.parse(flowerData),
 			}
-		}
+		},
 	}
 </script>
 
@@ -40,5 +73,11 @@
 	.sort-table__title {
     	font-size: 1.1em;
     	font-family: var(--main-font);
+	}
+
+	.table {
+		background-color: white;
+		height: 70%;
+		width: 80%;
 	}
 </style>
