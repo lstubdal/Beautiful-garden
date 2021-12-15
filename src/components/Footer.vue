69d71a785dbfccb7434ba3d5147d3d58dfbcc94e
lottestu <lottestu@student.matnat.uio.no>
2021-12-15 10:14:14 +0100

    Add comments and fix formatting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,19 @@
 <template>
     <footer class="footer">
         <nav class="footer__nav">
-            <RouterLink class="footer__page" v-for="page in navigationPages" :to="{ name: page.title, params: {page_id: page.id }}">{{ page.title }}</RouterLink>
+            <RouterLink class="footer__page" v-for="page in navigationPages" :to="{ name: page.title, params: {page_id: page.id }}">{{ page.title }}</RouterLink>   <!-- Use routerlink to navigate, using :to prop to specify location -->
         </nav>
     </footer>
 </template>
 
 <script>
-
     export default {
         computed: {
             navigationPages() {
-                return this.$store.getters.getPages;
+                return this.$store.getters.getPages;        /* get pages from store.js getter method */
             }
         } 
-    }
+    };
 </script>
 
 <style>
@@ -36,14 +35,14 @@
         text-decoration: none;
         width: 10%;  
         background-color: red;
-
     }
+
     .footer__logo-img {
         width: 10%;
         height: 10%;
-        padding: 0.5em;
-        
+        padding: 0.5em; 
     }
+
     .footer__page {
         font-family: arial;
         text-decoration: none;
@@ -51,6 +50,7 @@
         padding-left: 15px;
         padding-right: 15px;
     }
+    
     .footer__page:hover {
         color: var(--main-color);
         text-decoration: underline 2px solid;
